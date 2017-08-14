5763c31d2bff7573f784d9ac24fd6ba2321c0f1d
Arran France <arranf@gmail.com>
2017-08-14 09:47:53 +0100

    Allow leaders to impersonate a coordinator.

--- Plugins/org_kcionline/FollowUp/LineFollowUpList.ascx.cs
+++ Plugins/org_kcionline/FollowUp/LineFollowUpList.ascx.cs
@@ -1,20 +1,3 @@
-// <copyright>
-// Copyright 2013 by the Spark Development Network
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </copyright>
-//
-
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -25,6 +8,7 @@ using org.kcionline.bricksandmortarstudio.Utils;
 using OfficeOpenXml.FormulaParsing.Exceptions;
 using Rock;
 using Rock.Attribute;
+using Rock.Web.Cache;
 using Rock.Web.UI.Controls;
 
 namespace org_kcionline.FollowUp
@@ -118,10 +102,37 @@ namespace org_kcionline.FollowUp
         /// </summary>
         private void BindGrid()
         {
-            var connectionRequests = LineQuery.GetPeopleInLineFollowUpRequests(CurrentPerson);
 
             var rockContext = new RockContext();
             var groupMemberService = new GroupMemberService( rockContext );
+            var searchPerson = CurrentPerson;
+
+            // If the person is a group leader, get their coordinator so they can view as them
+            var consolidatorCoordinatorGuid = org.kcionline.bricksandmortarstudio.SystemGuid.GroupTypeRole.CONSOLIDATION_COORDINATOR.AsGuid();
+            var cellGroupType = GroupTypeCache.Read( org.kcionline.bricksandmortarstudio.SystemGuid.GroupType.CELL_GROUP.AsGuid() );
+            if (groupMemberService.Queryable().Any(gm => gm.GroupRole.IsLeader && gm.Group.GroupTypeId == cellGroupType.Id))
+            {
+                var baseGroup =
+                    // ReSharper disable once PossibleNullReferenceException
+                    groupMemberService.Queryable()
+                                      .FirstOrDefault(
+                                          gm => gm.GroupRole.IsLeader && gm.Group.GroupTypeId == cellGroupType.Id)
+                                      .Group;
+                while (baseGroup != null)
+                {
+
+                    var coordinator = baseGroup.Members.FirstOrDefault(gm => gm.GroupRole.Guid == consolidatorCoordinatorGuid);
+                    if (coordinator != null)
+                    {
+                        searchPerson = coordinator.Person;
+                        break;
+                    }
+                    baseGroup = baseGroup.ParentGroup;
+                }
+            }
+
+            var connectionRequests = LineQuery.GetPeopleInLineFollowUpRequests(searchPerson);
+
             var groupTypeRoleService = new GroupTypeRoleService(rockContext);
             var groupTypeRole = groupTypeRoleService.Get(org.kcionline.bricksandmortarstudio.SystemGuid.GroupTypeRole.CONSOLIDATED_BY.AsGuid());
 
