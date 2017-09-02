34d1b0dc8d3d40dce024e31ab713983cfa278fee
Arran France <arranf@gmail.com>
2017-09-02 18:47:14 +0100

    First stab at a data select to get a Line Leader, needs an attribute to be migrated on KCI Small Group members and a job to populate them,

---
+++ Reporting/DataSelect/Person/LineLeader.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Reporting;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace org.kcionline.bricksandmortarstudio.Reporting.DataSelect.Person
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Description( "Selects the full name of the person's line leader" )]
+    [Export( typeof( DataSelectComponent ) )]
+    [ExportMetadata( "ComponentName", "Select Person's Line Leader" )]
+    public class LineLeaderSelect : DataSelectComponent
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the name of the entity type. Filter should be an empty string
+        /// if it applies to all entities
+        /// </summary>
+        /// <value>
+        /// The name of the entity type.
+        /// </value>
+        public override string AppliesToEntityType
+        {
+            get
+            {
+                return typeof( Rock.Model.Person ).FullName;
+            }
+        }
+
+        /// <summary>
+        /// Gets the section that this will appear in in the Field Selector
+        /// </summary>
+        /// <value>
+        /// The section.
+        /// </value>
+        public override string Section
+        {
+            get
+            {
+                return base.Section;
+            }
+        }
+
+        /// <summary>
+        /// The PropertyName of the property in the anonymous class returned by the SelectExpression
+        /// </summary>
+        /// <value>
+        /// The name of the column property.
+        /// </value>
+        public override string ColumnPropertyName
+        {
+            get
+            {
+                return "LineLeaderName";
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the column field.
+        /// </summary>
+        /// <value>
+        /// The type of the column field.
+        /// </value>
+        public override Type ColumnFieldType
+        {
+            get { return typeof( string ); }
+        }
+
+        /// <summary>
+        /// Comma-delimited list of the Entity properties that should be used for Sorting. Normally, you should leave this as null which will make it sort on the returned field
+        /// To disable sorting for this field, return string.Empty;
+        /// </summary>
+        /// <param name="selection"></param>
+        /// <returns></returns>
+        /// <value>
+        /// The sort expression.
+        /// </value>
+        public override string SortProperties( string selection )
+        {
+            // disable sorting on this column since it is an IEnumerable
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Gets the default column header text.
+        /// </summary>
+        /// <value>
+        /// The default column header text.
+        /// </value>
+        public override string ColumnHeaderText
+        {
+            get
+            {
+                return "Line Leader's Name";
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets the title.
+        /// </summary>
+        /// <param name="entityType"></param>
+        /// <returns></returns>
+        /// <value>
+        /// The title.
+        /// </value>
+        public override string GetTitle( Type entityType )
+        {
+            return "Line Leader's Name";
+        }
+
+        /// <summary>
+        /// Gets the expression.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="entityIdProperty">The entity identifier property.</param>
+        /// <param name="selection">The selection.</param>
+        /// <returns></returns>
+        public override Expression GetExpression( RockContext context, MemberExpression entityIdProperty, string selection )
+        {
+            var kciSmallGroupGuid = SystemGuid.GroupType.CELL_GROUP.AsGuid();
+            // TODO Change
+            var attributeId = 1;
+
+            var leaderAttributes = new AttributeValueService(context)
+                .Queryable()
+                .Where(av => av.AttributeId == attributeId);
+            
+            var personService = new PersonService(context).Queryable();
+
+            var lineLeaderQuery = new PersonService(context)
+                .Queryable()
+                .Select(p => leaderAttributes
+                    .Where(a => a.EntityId == p.Id)
+                    .Select(a => personService
+                        .AsQueryable()
+                        .FirstOrDefault(q => q.Id == a.ValueAsPersonId)
+                        .FullName
+                    )
+                );
+
+            var selectLineLeaderExpression = SelectExpressionExtractor.Extract( lineLeaderQuery, entityIdProperty, "p" );
+
+            return selectLineLeaderExpression;
+        }
+
+        /// <summary>
+        /// Creates the child controls.
+        /// </summary>
+        /// <param name="parentControl"></param>
+        /// <returns></returns>
+        public override System.Web.UI.Control[] CreateChildControls( System.Web.UI.Control parentControl )
+        {
+            return new System.Web.UI.Control[] { };
+        }
+
+        /// <summary>
+        /// Gets the selection.
+        /// </summary>
+        /// <param name="controls">The controls.</param>
+        /// <returns></returns>
+        public override string GetSelection( System.Web.UI.Control[] controls )
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Sets the selection.
+        /// </summary>
+        /// <param name="controls">The controls.</param>
+        /// <param name="selection">The selection.</param>
+        public override void SetSelection( System.Web.UI.Control[] controls, string selection )
+        {
+           // Empty
+        }
+
+        #endregion
+    }
+}
